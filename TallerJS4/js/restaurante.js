e1e6cd933bb27e6b86291a1bd3e9137834464e85
LauraBenavidez <129073960+LauraBenavidez@users.noreply.github.com>
2023-10-23 18:07:18 -0500

    js
    js

--- TallerJS4/js/restaurante.js
+++ TallerJS4/js/restaurante.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Cuando la página web esté completamente cargada, se ejecutará lo siguiente:
+
     const orderForm = document.getElementById("order-form");
     const orderSummary = document.getElementById("order-summary");
     const totalPrice = document.getElementById("total-price");
 
+    // Encontramos tres lugares importantes en la página:
+    // - "orderForm" es donde se hacen los pedidos.
+    // - "orderSummary" es donde se muestra lo que pediste.
+    // - "totalPrice" es donde se muestra cuánto cuesta en total.
+
     let total = 0;
+    // Al principio, no hemos gastado dinero.
 
     orderForm.addEventListener("submit", function (event) {
+        // Si intentamos enviar el pedido, detenlo.
         event.preventDefault();
     });
 
     document.getElementById("add-to-order").addEventListener("click", function () {
+        // Cuando hacemos clic en el botón "Agregar al pedido", hacemos lo siguiente:
+
         const dish = document.getElementById("select-dish").value;
         const dishQuantity = parseInt(document.getElementById("dish-quantity").value);
         const drink = document.getElementById("select-drink").value;
         const drinkQuantity = parseInt(document.getElementById("drink-quantity").value);
+
+        // 1. Miramos qué comida y cuánta comida eligieron.
+        // 2. Miramos qué bebida y cuánta bebida eligieron.
 
         const dishPrice = {
             "Pizza hawaina": 12000,
@@ -31,14 +45,21 @@ document.addEventListener("DOMContentLoaded", function () {
             "Cerezada": 8000,
         }[drink] || 0;
 
+        // 3. Descubrimos cuánto cuesta la comida y la bebida.
+
         const subtotal = (dishPrice * dishQuantity) + (drinkPrice * drinkQuantity);
+
+        // 4. Calculamos cuánto cuesta en total.
 
         const orderItem = document.createElement("div");
         orderItem.classList.add("order-item");
         orderItem.innerHTML = ` ${dish} (x${dishQuantity}), ${drink} (x${drinkQuantity})`;
 
+        // 5. Mostramos lo que pidieron en la lista de pedidos.
+
         orderSummary.appendChild(orderItem);
 
+        // 6. Sumamos el costo al total.
         total += subtotal;
         totalPrice.textContent = total.toFixed();
     });
