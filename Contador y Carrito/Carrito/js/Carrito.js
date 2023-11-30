7276a8514e862cb1ff56bc721ed8c9b9ef6bb107
LauraBenavidez <129073960+LauraBenavidez@users.noreply.github.com>
2023-11-30 10:06:56 -0500

    JS

--- Contador y Carrito/Carrito/js/Carrito.js
+++ Contador y Carrito/Carrito/js/Carrito.js
@@ -1,23 +1,50 @@
+// Función para calcular el subtotal de un libro
+function calcularSubtotal(cantidadElementId, precioElementId, subtotalElementId) {
+    var cantidad = parseInt(document.getElementById(cantidadElementId).value, 10);
+    var precio = parseInt(document.getElementById(precioElementId).innerText.replace(/\./g, ''), 10);
+    var subtotal = cantidad * precio;
+    document.getElementById(subtotalElementId).innerText = "Subtotal: $" + subtotal;
+    return subtotal;
+}
+
 // Función para calcular el total
 function calcularTotal() {
-    // Obtener los elementos HTML
-    var cantidadLibro1 = parseInt(document.getElementById("cantidadLibro1").value, 10);
-    var precioLibro1 = parseInt(document.getElementById("precioLibro1").innerText.replace(/\./g, ''), 10);
+    var subtotalLibro1 = calcularSubtotal("cantidadLibro1", "precioLibro1", "subtotalLibro1");
+    var subtotalLibro2 = calcularSubtotal("cantidadLibro2", "precioLibro2", "subtotalLibro2");
 
-    var cantidadLibro2 = parseInt(document.getElementById("cantidadLibro2").value, 10);
-    var precioLibro2 = parseInt(document.getElementById("precioLibro2").innerText.replace(/\./g, ''), 10);
+    var totalLibros = subtotalLibro1 + subtotalLibro2;
+    document.getElementById("totalLibros").innerText = totalLibros;
 
-    // Calcular el subtotal de cada libro y sumarlos para obtener el total
-    var subtotalLibro1 = cantidadLibro1 * precioLibro1;
-    var subtotalLibro2 = cantidadLibro2 * precioLibro2;
-
-    var total = subtotalLibro1 + subtotalLibro2;
-
-    // Mostrar el total en el elemento HTML correspondiente
-    document.getElementById("total").innerText = formatearNumero(total);
+    var totalGeneral = totalLibros;  // Puedes ajustar aquí si hay más libros en el futuro
+    document.getElementById("total").innerText = totalGeneral;
 }
 
-// Función para formatear el número con separadores de miles
-function formatearNumero(numero) {
-    return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+// Función para sumar a la cantidad
+function sumarCantidad(elementId, precioElementId, subtotalElementId) {
+    var cantidadElement = document.getElementById(elementId);
+    cantidadElement.value = parseInt(cantidadElement.value, 10) + 1;
+    var subtotal = calcularSubtotal(elementId, precioElementId, subtotalElementId);
+    calcularTotal();
+    return subtotal;
 }
+
+// Función para restar a la cantidad
+function restarCantidad(elementId, precioElementId, subtotalElementId) {
+    var cantidadElement = document.getElementById(elementId);
+    var cantidadActual = parseInt(cantidadElement.value, 10);
+    if (cantidadActual > 1) {
+        cantidadElement.value = cantidadActual - 1;
+        var subtotal = calcularSubtotal(elementId, precioElementId, subtotalElementId);
+        calcularTotal();
+        return subtotal;
+    }
+}
+
+// Agregar eventos de clic a los botones de suma y resta
+document.getElementById("sumarLibro1").addEventListener("click", function() { sumarCantidad("cantidadLibro1", "precioLibro1", "subtotalLibro1"); });
+document.getElementById("restarLibro1").addEventListener("click", function() { restarCantidad("cantidadLibro1", "precioLibro1", "subtotalLibro1"); });
+document.getElementById("sumarLibro2").addEventListener("click", function() { sumarCantidad("cantidadLibro2", "precioLibro2", "subtotalLibro2"); });
+document.getElementById("restarLibro2").addEventListener("click", function() { restarCantidad("cantidadLibro2", "precioLibro2", "subtotalLibro2"); });
+
+// Llamar a la función calcularTotal al cargar la página
+window.onload = calcularTotal;
